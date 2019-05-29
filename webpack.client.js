ec372dd6ec9a1287cc716c39dc4d1c55a425a20d
Leisan Gilfanova <allarepossible@gmail.com>
2019-05-29 20:33:01 +0300

    merge webpack configs

--- webpack.client.js
+++ webpack.client.js
@@ -1,24 +1,15 @@
 const path = require('path');
 
-module.exports = {
+const merge = require('webpack-merge');
+
+const baseConfig = require('./webpack.base');
+
+const config = {
     entry: './src/client/client.js',
     output: {
         path: path.join(__dirname, 'public'),
         filename: 'bundle.js',
     },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                options: {
-                    presets: [
-                        '@babel/preset-env',
-                        '@babel/preset-react',
-                    ],
-                },
-            },
-        ],
-    },
 };
+
+module.exports = merge(baseConfig, config);
--- webpack.server.js
+++ webpack.server.js
@@ -1,25 +1,16 @@
 const path = require('path');
 
-module.exports = {
+const merge = require('webpack-merge');
+
+const baseConfig = require('./webpack.base');
+
+const config = {
     target: 'node',
     entry: './server/index.js',
     output: {
         path: path.join(__dirname, '/dist'),
         filename: 'bundle.js',
     },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                options: {
-                    presets: [
-                        '@babel/preset-env',
-                        '@babel/preset-react',
-                    ],
-                },
-            },
-        ],
-    },
 };
+
+module.exports = merge(baseConfig, config);
