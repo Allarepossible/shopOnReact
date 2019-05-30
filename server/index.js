341d6264b044506f5788abb39187ba07606b92c8
Leisan Gilfanova <allarepossible@gmail.com>
2019-05-30 15:15:09 +0300

    add react-route-config but it doesn't work

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import 'idempotent-babel-polyfill';
 import express from 'express';
+import {matchRoutes} from 'react-router-config';
 
+import Routes from '../src/client/Routes';
 import renderer from '../src/helpers/renderer';
 import createStore from '../src/helpers/createStore';
 
@@ -11,6 +13,8 @@ app.use(express.static('public'));
 
 app.get('*', (req, res) => {
     const store = createStore();
+
+    console.log(matchRoutes(Routes, req.path))
 
     res.send(renderer(req, store));
 });
--- src/client/client.js
+++ src/client/client.js
@@ -1,7 +1,8 @@
 import 'idempotent-babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Switch} from 'react-router-dom';
+import {renderRoutes} from 'react-router-config';
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import {Provider} from 'react-redux';
@@ -14,7 +15,9 @@ const store = createStore(reducers, {}, applyMiddleware(thunk));
 ReactDOM.hydrate(
     <Provider store={store}>
         <BrowserRouter>
-            <Routes />
+            <Switch>
+                <div>{renderRoutes(Routes)}</div>
+            </Switch>
         </BrowserRouter>
     </Provider>,
     document.querySelector('#root')
