e845b751919cfd20113c334b887232fddc364a78
Leisan Gilfanova <allarepossible@gmail.com>
2019-05-31 14:47:56 +0300

    add hocs and redirect on client

--- server/index.js
+++ server/index.js
@@ -28,6 +28,12 @@ app.get('*', (req, res) => {
 
     const promises = matchRoutes(Routes, req.path).map(({route}) => {
         return route.loadData ? route.loadData(store) : null;
+    }).map(promise => {
+        if (promise) {
+            return new Promise((resolve, reject) => {
+                promise.then(resolve).catch(resolve);
+            });
+        }
     });
 
     Promise.all(promises).then(() => {
