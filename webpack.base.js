e6dc290d11dba2c13a876767dd50975539830268
Leisan Gilfanova <allarepossible@gmail.com>
2019-06-08 00:20:33 +0300

    first step to type in js

--- webpack.base.js
+++ webpack.base.js
@@ -1,11 +1,16 @@
 module.exports = {
     mode: 'development',
     module: {
+        resolve: {extensions: ['.js', '.jsx', '.react.js', '.ts', '.tsx']},
         rules: [
             {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
+                // test: /\.js$/,
+                // exclude: /node_modules/,
+                // loader: 'babel-loader',
+            },
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader',
             },
             {
                 test: /\.(pdf|jpg|png|gif|svg|ico)$/,
