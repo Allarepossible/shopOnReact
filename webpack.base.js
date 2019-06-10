8785b56ccf37c5b53b553a6282aff8a062c5cc49
Leisan Gilfanova <allarepossible@gmail.com>
2019-06-10 19:54:33 +0300

    second step

--- webpack.base.js
+++ webpack.base.js
@@ -7,9 +7,9 @@ module.exports = {
     module: {
         rules: [
             {
-                // test: /\.js$/,
-                // exclude: /node_modules/,
-                // loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
             },
             {
                 test: /\.tsx?$/,
