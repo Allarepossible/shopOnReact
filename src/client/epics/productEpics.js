bd8dfd92c976f7b4eefde71f1f8d2ca4a481e157
Leisan Gilfanova <allarepossible@gmail.com>
2019-07-23 22:41:55 +0300

    add ignore elements

--- src/client/epics/productEpics.js
+++ src/client/epics/productEpics.js
@@ -1,14 +1,14 @@
 import {ofType} from 'redux-observable';
-import {mergeMap, map} from 'rxjs/operators';
+import {mergeMap, map, ignoreElements} from 'rxjs/operators';
 
 import {FETCH_PRODUCT, fetchProductDone} from '../actions/product';
 
 export const fetchProductEpic = action$ => action$.pipe(
     ofType(FETCH_PRODUCT),
-    mergeMap(({payload}) => {
+    map(({payload}) => {
         const {api, path} = payload;
 
         return api.get(path);
     }),
-    map(({data}) => fetchProductDone(data))
+    ignoreElements()
 );
