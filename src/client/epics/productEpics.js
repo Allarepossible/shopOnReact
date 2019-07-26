eb98966e331e1f72452ab6cdc414a8f4c7a75c91
Leisan Gilfanova <allarepossible@gmail.com>
2019-07-26 23:13:16 +0300

    add observable

--- src/client/epics/productEpics.js
+++ src/client/epics/productEpics.js
@@ -1,5 +1,5 @@
+import {Observable} from 'rxjs';
 import {ofType} from 'redux-observable';
-import {of} from 'rxjs';
 import {mergeMap, ignoreElements} from 'rxjs/operators';
 
 import {FETCH_PRODUCT, fetchProductDone} from '../actions/product';
@@ -10,7 +10,7 @@ export const fetchProductEpic = action$ => action$.pipe(
         const {api, path} = payload;
 
         return api.get(path)
-            .mergeMap(({data}) => of(fetchProductDone(data)))
+            .then(({data}) => Observable.of(fetchProductDone(data)))
             .catch(error => {
                 console.log('caught', error.message);
             });
